a69cf389e1565ce705cfd253348a36c3f7a6c2e3
sanjeev-ramakumar <sanjeev.ramakumar@gmail.com>
2014-07-17 00:08:23 -0700

    add zing library

--- PregSafe/src/com/pregsafe/HomeActivity.java
+++ PregSafe/src/com/pregsafe/HomeActivity.java
@@ -1,15 +1,51 @@
 package com.pregsafe;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeActivity extends Activity {
 
+    private Button mScan;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		
+		super.onCreate(savedInstanceState);		
 		setContentView(R.layout.home);
+		
+        mScan = (Button) findViewById(R.id.scan_button);
+        mScan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent("com.google.zxing.client.android.SCAN");
+                startActivityForResult(intent, 0);
+            }
+        });		
 	}
+	
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 0) {
+            if (resultCode == RESULT_OK) {
+                String contents = data.getStringExtra("SCAN_RESULT");
+//                String format = data.getStringExtra("SCAN_RESULT_FORMAT");
 
+                // Handle successful scan
+                Log.i("PREGSAFE", "Barcode = " + contents);
+
+                // Show Product page
+                Intent productIntent = new Intent(this, ProductActivity.class);
+                productIntent.putExtra("com.pregsafe.BARCODE", contents);
+                startActivity(productIntent);
+
+            } else if (resultCode == RESULT_CANCELED) {
+                // Handle Cancel
+                Log.i("PREGSAFE", "Scan unsuccessful");
+            }
+        }
+    }
+	
 }
